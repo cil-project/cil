8c68b1464a20f4a182e649e1f528b5acf3386a4e
Westley Weimer <weimer@cs.berkeley.edu>
2001-04-24 18:06:41 +0000

    more test cases

---
+++ test/small1/struct_init.c
@@ -0,0 +1,26 @@
+typedef struct {
+    char * name;
+    int data;
+} cmd;
+
+cmd our_cmds[] = {
+    { "command 1", 1 },
+    { "command 2", 2 },
+    { "command 3", 3 },
+    { 0, 0} };
+
+struct {
+    int x;
+    cmd * cmds;
+    int y;
+} main_struct = { 
+    100,
+    our_cmds,
+    200
+};
+
+int main() {
+    char * FSEQN p = "HELLO";
+    return 0;
+}
+
