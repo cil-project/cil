45d79b2f83dd278ef3b12a9f4a8263da889cdcc8
Westley Weimer <weimer@cs.berkeley.edu>
2001-05-07 02:59:43 +0000

    problem with #pragma box(off)

---
+++ test/small1/huff1.c
@@ -0,0 +1,17 @@
+unsigned short read2Target(unsigned char * ptr )
+{
+    if (1 /* targetNotLikeHost */) {
+        return ((unsigned short )(((int )(*ptr) << 8) + (int )(*(ptr + 1))));
+    } else {
+        return ((*((unsigned short *)ptr)));
+    }
+}
+
+#pragma box(off)
+int  readStructTarget(unsigned char * filePtr ,
+                      unsigned char * fileEnd , ...) {
+    int x = read2Target(fileEnd);
+    return x;
+}
+#pragma box(on)
+
