3bf39045b22aba2661c91bc27cfc47911f11d04e
Aman Bhargava <amanb@cs.berkeley.edu>
2001-06-19 00:13:26 +0000

    Added MARSHALREAD feature. DEPTH, WIDTH and MODE env vars are now capitalized

--- src/prettytest.ml
+++ src/prettytest.ml
@@ -51,18 +51,37 @@ let treeTest n colWidth =
   Pretty.noBreaks := false;
   Pretty.noAligns := false;
   Random.init 10;
-  
-  let t = makeStringTree n in 
-  ignore (Printf.fprintf stderr "Mode = %s Tree size = %d \t ColWidth = %d\n" (Sys.getenv "Mode") (countLeaves t) colWidth);
-  ignore (flush stderr);
 
-  let mode = Sys.getenv "Mode" in
+  ignore (Pretty.fprintf stderr "prettytest: Depth = %d " n);
+  ignore (flush stderr);
+  let mode = try Sys.getenv "MODE" with Not_found -> "" in
+  let t = makeStringTree n in 
+  ignore (Printf.fprintf stderr "Mode = %s Tree size = %d \t ColWidth = %d\n" mode (countLeaves t) colWidth);
+  ignore (flush stderr);
   if mode <> "SkipPrint" then 
     Pretty.fprint stdout colWidth (tree2doc t)
-	    
+	
+
+let doTest () =
+  let width = try (int_of_string (Sys.getenv "WIDTH")) with Not_found -> 80 in
+  let depth = try (int_of_string (Sys.getenv "DEPTH")) with Not_found -> 9 in
+  let marshalFilename = 
+    try Sys.getenv "MARSHALREAD"       
+    with Not_found -> (treeTest depth width) ; exit 0
+  in
+  Printf.fprintf stderr "Marshalling in %s\n" marshalFilename;
+  let chn = open_in_bin marshalFilename in
+  let fcount = ref 0 in
+  try
+    while true do
+      Pretty.fprint stdout width (Obj.magic (Marshal.from_channel chn) : doc);
+      fcount := !fcount + 1
+    done
+  with End_of_file -> begin
+    Printf.fprintf stderr "%d documents printed from marshaled file\n" !fcount;
+    ()
+  end
 ;;
 
-(treeTest 
-   (int_of_string (Sys.getenv "Depth"))
-   (int_of_string (Sys.getenv "Width")));;
+doTest ();;
 
