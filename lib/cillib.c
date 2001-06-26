57cc72f05a439185903ec809bbb74e206a7f5236
Scott McPeak <smcpeak@cs.berkeley.edu>
2001-06-26 12:08:19 +0000

    cleaned up _get__ctype_b wrapper li-cil works

--- lib/cillib.c
+++ lib/cillib.c
@@ -1,5 +1,9 @@
-#include <ctype.h>
-#include <stdio.h>
+// cillib.c
+// stuff that gets linked with cil'd programs *and* box'd programs
+
+#include <ctype.h>      // __ctype_b
+#include <stdio.h>      // FILE, fprintf
+#include <stdlib.h>     // abort
 
 FILE * get_stdin() {
     return stdin;
@@ -17,9 +21,14 @@ int _get_ctype_p1(unsigned int chr) {
 }
 #endif
 
-#if defined(__GLIBC__) && __GLIBC__ == 2 && __GLIBC_MINOR__ == 0
+#if defined(__GLIBC__) && __GLIBC__ == 2
 /// For brooksie
+// sm: glibc 2.0 and 2.1 both have this; so I removed check for minor number
 int _get__ctype_b(int chr) { 
+  if (!( (unsigned)chr < 256 )) {
+    fprintf(stderr, "Bad character index to _get__ctype_b: %d\n", chr);
+    abort();
+  }
   return (__ctype_b[chr]);
 }
 #endif
