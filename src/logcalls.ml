b88422ccf6992c1124aee00bcf2b847001331477
Westley Weimer <weimer@cs.berkeley.edu>
2001-10-24 20:22:52 +0000

    small wessy changes

--- src/logcalls.ml
+++ src/logcalls.ml
@@ -22,17 +22,23 @@ class verboseLogVisitor printfFun funstr = object
   method vinst i = begin
     match i with
       Call(lo,e,al,l) -> 
-      let str1 = Pretty.sprint 800 ( Pretty.dprintf "Calling %a(%a)\n" d_exp e
-        (docList (chr ',') (fun arg -> 
+      let str1 = Pretty.sprint 80 ( Pretty.dprintf "Calling %a(@[%a@])\n" d_exp e
+        (docList (chr ',' ++ break ) (fun arg -> 
           try
-            match typeOf arg with
+            match unrollType (typeOf arg) with
                 TInt _ | TEnum _ -> dprintf "%a = %%d" d_exp arg
               | TFloat _ -> dprintf "%a = %%g" d_exp arg
+              | TVoid _ -> text "void"
+              | TComp _ -> text "comp"
               | _ -> dprintf "%a = %%p" d_exp arg
           with  _ -> dprintf "%a = %%p" d_exp arg)) al) in
+      let log_args = List.filter (fun arg ->
+        match unrollType (typeOf arg) with
+          TVoid _ | TComp _ -> false
+        | _ -> true) al in 
       let str2 = Pretty.sprint 800 ( Pretty.dprintf "Returned from %a\n" d_exp e) in
       let newinst str args = ((Call (None, Lval(var printfFun.svar),
-                                ( [ one ; Const(CStr(str)) ] @ args),
+                                ( [ one ; Const(CStr(str)) ] @ log_args),
                                 locUnknown)) : instr )in
       let ilist = ([ (newinst str1 al) ; i ; (newinst str2 []) ] : instr list) in
     (ChangeTo(ilist))
@@ -77,13 +83,18 @@ let logCalls (f: file) : unit =
       GFun (fdec, loc) -> 
         (* Collect expressions that denote the actual arguments, and a format 
          * string for printing them *)
-        let actargs = List.map (fun vi -> Lval(var vi)) fdec.sformals in
+        let actargs = List.map (fun vi -> Lval(var vi)) 
+          (List.filter (fun vi -> match unrollType vi.vtype with
+            TVoid _ | TComp _ -> false 
+            | _ -> true) fdec.sformals) in
         let formatstr = Pretty.sprint 60
           (dprintf "entering %s(%a)\n" fdec.svar.vname
             (docList (chr ',' ++ break) 
-            (fun vi -> match vi.vtype with
+            (fun vi -> match unrollType vi.vtype with
               TInt _ | TEnum _ -> dprintf "%s = %%d" vi.vname
             | TFloat _ -> dprintf "%s = %%g" vi.vname
+            | TVoid _ -> dprintf "%s = void" vi.vname
+            | TComp _ -> dprintf "%s = comp" vi.vname
             | _ -> dprintf "%s = %%p" vi.vname)) fdec.sformals) in
         i := 0 ;
         name := fdec.svar.vname ; 
