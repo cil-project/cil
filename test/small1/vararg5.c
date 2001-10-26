3465b18b80a9693e354138c2208b3e1b217e0363
jiggaman <jiggaman@cs.berkeley.edu>
2001-10-26 09:43:42 +0000

    added vararg5.c to the test/small1 directory

---
+++ test/small1/vararg5.c
@@ -0,0 +1,29 @@
+/* 
+This file is a test for variable argument functions, where the call to 
+print is made in a separate function, talking. This checks to see if we 
+can retrieve the arguments in a variable argument function if we call a 
+separate function.
+*/
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+
+void chatting(char* s, ...);
+
+int main(int argc, char** argv) {
+  int x, y;
+  char* s;
+  x = 5;
+  y = 1;
+  s = "hello";
+  talking("This should be 5: %d\n", x);
+  return 0;
+}
+
+void talking(char* s, ...) {
+  va_list ap;
+  va_start(ap, s);
+  vfprintf(stdout, s, ap);
+  va_end(ap);
+}
