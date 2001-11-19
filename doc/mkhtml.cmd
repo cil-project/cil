ea60720219421b4aefc893485bf6ca253db1fb4a
George Necula <necula@cs.berkeley.edu>
2001-11-19 00:17:09 +0000

    Reorganized the documentation

--- doc/mkhtml.cmd
+++ doc/mkhtml.cmd
@@ -1,15 +1,40 @@
-@echo off
-set HEVEADIR=%PROGRAMS%\hevea
-set OPATH=%PATH%
-set PATH=%OPATH%;%HEVEADIR%
+#!/bin/sh
 
-@echo on
 cd html
-%HEVEADIR%\hevea -exec xxdate.exe ../manual
-%HEVEADIR%\hevea -exec xxdate.exe ../manual
-%HEVEADIR%\hacha -o toc.html manual.html
+rm -f *
 
-%HEVEADIR%\hevea -exec xxdate.exe ../setup
-%HEVEADIR%\hevea -exec xxdate.exe ../setup
+
+set HEVEADIR=C:\Programs\hevea
+
+hevea -exec xxdate.exe ../setup
+hevea -exec xxdate.exe ../setup
+
+hevea -exec xxdate.exe ../cil
+hevea -exec xxdate.exe ../cil
+hacha -o ciltoc.html cil.html
+cp ../cilindex.html .
+cp ../cilheader.html . 
+
+
+hevea -exec xxdate.exe ../ccured
+hevea -exec xxdate.exe ../ccured
+hacha -o ccuredtoc.html ccured.html
+cp ../ccuredindex.html .
+cp ../ccuredheader.html . 
+
+
+rm -rf ccured
+mkdir ccured
+mkdir ccured\cil
+
+cp ccured*.html ccured
+mv ccured/ccuredindex.html ccured/index.html
+cp *.gif ccured
+
+cp cil*.html ccured/cil
+cp *.gif ccured/cil 
+mv ccured/cil/cilindex.html ccured/cil/index.html
+cp setup*.html ccured/cil
+
 
 cd ..
--- doc/publish.cmd
+++ doc/publish.cmd
@@ -2,16 +2,11 @@
 call mkhtml.cmd
 
 cd html
-REM Copy everything to a ccured directory 
-mkdir ccured
-copy *.html ccured
-copy *.gif ccured
-cp -r cil ccured
-tar cvfz ccured.taz ccured/*
 
+tar cvfz ccured.taz ccured/*
+    
 set upload=perl C:/Necula/bin/Perl/upload.pl
 set dest=http://raw.cs.berkeley.edu/cgi/upload.cmd
 %upload% %dest%   ccured.taz
 
-rm -rf ccured
 cd ..
