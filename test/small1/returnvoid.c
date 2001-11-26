fbf7c3733c24be077c529b4b8d8c15bd3a745758
Winston Liaw <winston@cs.berkeley.edu>
2001-11-26 03:15:54 +0000

    Added returnvoid.c and returnvoid1.c. Returnvoid is a simple test case illustrating a problem that occurs when functions returning void return the value of another function (i.e the body of h() might consist of return(g()), and g() returns void.)Hard to explain, for clarification, just look at returnvoid.c =). Returnvoid1.c is a more complicated test case, but it shows the same thing. The problem that cil has is that it inserts a tmp variable of type void, and then proceeds to set a value to that tmp variable, which it cannot do.

---
+++ test/small1/returnvoid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void g();
+void h();
+
+int main(int argc, char ** argv) {
+  h();
+  return 0;
+}
+
+void h() {
+  return(g());
+}
+
+void g() {
+  return;
+}
