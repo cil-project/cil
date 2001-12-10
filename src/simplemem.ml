75437da393dcd30dfeab5dbbc90854eeef2af60e
Westley Weimer <weimer@cs.berkeley.edu>
2001-12-10 23:55:34 +0000

    Finished touches on the "simplemem" transformation. I ran spec95 compress through it and got the same answer, so it looks OK.

--- src/simplemem.ml
+++ src/simplemem.ml
@@ -3,24 +3,25 @@
  * "simple". Introduce well-type temporaries to hold intermediate values
  * for expressions that would normally involve more than one memory
  * reference. 
+ *
+ * If simplemem succeeds, each lvalue should contain only one Mem()
+ * constructor. 
  *)
 open Cil
 
+(* current context: where should we put our temporaries? *)
 let thefunc = ref None 
 
-let assignment_list = ref [] 
+(* build up a list of assignments to temporary variables *)
+let assignment_list = ref (ref [])
 
-let target_type tau = 
-  match unrollType tau with
-  | TPtr(dest,_) -> dest
-  | TArray(dest,_,_) -> dest
-  | _ -> tau
-
+(* turn "int a[5][5]" into "int ** temp" *)
 let rec array_to_pointer tau = 
   match unrollType tau with
     TArray(dest,_,al) -> TPtr(array_to_pointer dest,al)
   | _ -> tau
 
+(* create a temporary variable in the current function *)
 let make_temp tau = 
   let tau = array_to_pointer tau in 
   match !thefunc with
@@ -36,18 +37,21 @@ let rec separate_loffsets lo =
       Field(fi,s) , m
   | Index(_) -> NoOffset, lo
 
+(* Recursively decompose the lvalue so that what is under a "Mem()"
+ * constructor is put into a temporary variable. *)
 let rec handle_lvalue (lb,lo) = 
   let s,m = separate_loffsets lo in 
   match lb with
     Var(vi) -> 
       handle_loffset (lb,s) m 
-  | Mem(Lval(Var(_),NoOffset)) -> 
+  | Mem(Lval(Var(_),NoOffset)) ->
+			(* special case to avoid generating "tmp = ptr;" *)
       handle_loffset (lb,s) m 
   | Mem(e) -> 
       begin
         let new_vi = make_temp (typeOf e) in
-        assignment_list := (new_vi, e, !currentLoc) 
-          :: ! assignment_list ;
+        !assignment_list := (new_vi, e, !currentLoc) 
+          :: ! (!assignment_list) ;
         handle_loffset (Mem(Lval(Var(new_vi),NoOffset)),NoOffset) lo
       end
 and handle_loffset lv lo = 
@@ -58,36 +62,42 @@ and handle_loffset lv lo =
   | Index(exp,o) -> 
       handle_loffset (addOffsetLval (Index(exp,NoOffset)) lv) o
 
-class simpleVisitor = object
+(* the transformation is implemented as a Visitor *)
+class simpleVisitor = object 
   inherit nopCilVisitor
 
-  method vfunc fundec =
+  method vfunc fundec = (* we must record the current context *)
     thefunc := Some(fundec) ;
     DoChildren
 
   method vlval lv = 
-    (* let res = handle_lvalue lv in *)
     ChangeDoChildrenPost(lv,
       (fun lv -> handle_lvalue lv))
 
   method vinst i = 
+		(* this "my_alist" idiocy is to make sure that temporary statements
+		 * that get generated to deal with, e.g., the predicate of an
+		 * 'if' statement get put before the 'if' statement and not inside
+		 * one of the branches *)
+		let my_alist = ref [] in
+		assignment_list := my_alist ;
     ChangeDoChildrenPost([i],
       (fun i_list -> 
         let new_instr_list = List.map (fun (vi,e,loc) ->
           Set((Var(vi),NoOffset),e,loc)) 
-            (List.rev !assignment_list) in
-        assignment_list := [] ;
+            (List.rev !my_alist) in
         new_instr_list @ i_list))
 
-  method vstmt s =
+  method vstmt s = 
+		let my_alist = ref [] in
+		assignment_list := my_alist ;
     ChangeDoChildrenPost(s,
       (fun s -> 
-        if !assignment_list = [] then s
+        if !my_alist = [] then s
         else begin
           let new_instr_list = List.map (fun (vi,e,loc) ->
             Set((Var(vi),NoOffset),e,loc)) 
-              (List.rev !assignment_list) in
-          assignment_list := [] ;
+              (List.rev !my_alist) in
           let new_stmt = mkStmt (Instr(new_instr_list)) in 
           let new_block = mkBlock ([new_stmt ; s ;]) in
           mkStmt (Block(new_block))
@@ -95,8 +105,9 @@ class simpleVisitor = object
     ))
 end
 
+(* Main entry point: apply the transformation to a file *)
 let simplemem (f : file) =
   try 
     visitCilFile (new simpleVisitor) f
-  with e -> Printf.printf "Error in simplemem: %s"
-    (Printexc.to_string e) ; f
+  with e -> Printf.printf "Exception in Simplemem.simplemem: %s\n"
+    (Printexc.to_string e) ; raise e
