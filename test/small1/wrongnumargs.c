474e3c1d809c25f777dddfda97255c455f589b35
George Necula <necula@cs.berkeley.edu>
2002-01-01 19:39:03 +0000

    Changed back wrongnumargs.c

--- test/small1/wrongnumargs.c
+++ test/small1/wrongnumargs.c
@@ -7,7 +7,7 @@ void f(s1, s2) char *s1, *s2;
 
 int main(int argc, char** argv) {
   f("hello %S!\n", "wow");	  
-  f("hello there: %s!\n", "wow", "hey there");
+  f("hello there: %s!\n", "wow");
   f("hello again!\n");
 }
 
