319cbdbd8e5e2c381b95dc7da70a7f5176a743de
George Necula <necula@cs.berkeley.edu>
2002-02-14 23:05:01 +0000

    A few bug fixes related to directory changes

--- test/testcil.pl
+++ test/testcil.pl
@@ -467,7 +467,7 @@ sub smAddTest {
 
   my %patterns = %commonerrors;
   my $tst = $self->newTest(Name => $tname,
-                           Dir => "..",
+                           Dir => ".",
                            Cmd => "make $command" . $self->testCommandExtras(""),
                            Group => ['quick'],
                            Patterns => \%patterns);
