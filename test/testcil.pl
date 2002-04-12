d58b2d6526c3a492d02d55e3e5beaa47a69674a8
Scott McPeak <smcpeak@cs.berkeley.edu>
2002-04-12 09:28:58 +0000

    added a testcase for an annoying gcc warning

--- test/testcil.pl
+++ test/testcil.pl
@@ -667,6 +667,7 @@ smAddTest("test/addrofparam $box");
 smAddTest("scott-nolink/name-capture-bitand $box");
 smAddTest("scott-nolink/wildfun2 $box");
 smAddTest("scott/dblarg.int $box");      # this yields a warning that might be a problem
+smAddTest("scott/decl_inl $box");        # produces a gcc warning I'd like to silence
 
 # tests of things implemented for EDG compatibility
 smAddTest("mergestruct");
