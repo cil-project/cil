7f373bd6aedc7dd35d7f8a0e90970936a30bf066
George Necula <necula@cs.berkeley.edu>
2002-06-24 22:46:53 +0000

    Turned off pkReachSeq in the wrappers

--- test/testcil.pl
+++ test/testcil.pl
@@ -382,6 +382,7 @@ $TEST->addTests("testrun/voidarg", "", ['cil']);
 $TEST->addTests("testrun/union2", "", ['cil']);
 $TEST->addTests("testrun/union3", "", ['cil']);
 $TEST->addTests("testrun/inline1", "", ['cil']);
+$TEST->addTests("testrun/tcast2", "", ['inferbox']);
 $TEST->addTests("testrun/pointerdiff", "", ['cil', 'inferbox', 'box']);
    $TEST->addBadComment("testrun/pointerdiff-inferbox", 
                         "A pointer that is not read should not be bound-checked");
