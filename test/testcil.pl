0e6397230736116ff7d301028add8bae8b5d0996
George Necula <necula@cs.berkeley.edu>
2002-08-07 16:58:53 +0000

    Turned off FSEQ to see the performance impact

--- test/testcil.pl
+++ test/testcil.pl
@@ -1097,7 +1097,7 @@ sub testCommandExtras {
         $theargs .= " NOREMAKE=1";
     }
     # Turn on the verbose flag
-    $theargs .= " STATS=1 PRINTSTAGES=1 ";
+    $theargs .= " STATS=1 PRINTSTAGES=1 EXTRAARGS=--noFSEQ ";
 
     return $theargs;
 }
