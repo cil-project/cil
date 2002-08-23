97f1d42f089d33a2c491e4b6b2b74fb726c46de7
George Necula <necula@cs.berkeley.edu>
2002-08-23 23:41:39 +0000

    Extended the browser to show an index of globals and types. IGNORE

--- test/small2/seq_align_malloc.c
+++ test/small2/seq_align_malloc.c
@@ -2,22 +2,15 @@
 //  Failure at ./Config_Test.mergedcured.c:open__112ACE_Malloc_T__tm__92_21ACE_Local_Memory_Pool29ACE_Local_Memory_Pool_Options16ACE_Thread_Mutex17ACE_Control_BlockFv_i:161967: Creating an unaligned sequence
 
 #include <stdlib.h>
-#include <assert.h>
-
-#define CCURED_TRUSTED_CAST(t, what) ((t)trusted_cast((void*)(what)))
 
 int main() {
     struct s {
         int one; int two; int three;
-    };
-    char *p;
-    struct s *s0;
-
-    assert(sizeof (struct s)==12);
-    p = malloc(15);
-    s0 = CCURED_TRUSTED_CAST(struct s *, p);
-    s0++;
-    s0--;
+    } *p;
+    void *v = malloc(15);
+    p = (struct s*)v; // Check here that there is enough space in p
+    p ++;
+    p--;
 
     return 0;
 }
