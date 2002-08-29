2bf09a04082371164a27d35bca4963ea1693f91b
Westley Weimer <weimer@cs.berkeley.edu>
2002-08-29 17:50:00 +0000

    Add a testcase from bind.

--- test/testcil.pl
+++ test/testcil.pl
@@ -336,6 +336,7 @@ $TEST->add3Tests("testrun/addrof", "MANUALBOX=1");
 $TEST->add3Tests("testrun/addrof2", "MANUALBOX=1");
 $TEST->addTests("testrun/addrof3", "_GNUCC=1", ['cil']);
 $TEST->add3Tests("testrun/lval1", "_GNUCC=1");
+$TEST->add3Tests("testrun/bind1", "EXTRAARGS=-assumePrintf");
 $TEST->add3Tests("testmodel/model1");
 $TEST->add3Tests("testmodel/modelpoly");
 $TEST->add2Tests("testwrapper/wrapper1");
