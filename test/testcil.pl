f479b80482d13c350823aeb815118b3bd6ca746c
Scott McPeak <smcpeak@cs.berkeley.edu>
2002-09-08 07:11:14 +0000

    continuing to work on ftpd; getopt is broken now that it refers to optarg, as ftpd does, so that's a TODO to fix

--- test/testcil.pl
+++ test/testcil.pl
@@ -860,6 +860,7 @@ smAddTest("scott/bsearch $box");
 smAddTest("scott/signal $box");
 smAddTest("scott/getaddrinfo $box");
 smAddTest("test-bad/sin_zero $box");
+smFailTest("optarg_F missing", "scott/getopt $box");
 
 # current problematic test cases
 smAddTest("scott/complex_float $box");
