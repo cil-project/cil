1365a1b1771802ea5b48ff727e6f3b750490ef04
Westley Weimer <weimer@cs.berkeley.edu>
2002-10-08 03:26:29 +0000

    Added the "bind-empty-chain" test which is very small but generates the message:   Warning: Chain for incompat 70 and 52 is empty I have been seeing huge numbers (pages) of similar warnings quite a bit recently and thought you might like a testcase.

--- test/testcil.pl
+++ test/testcil.pl
@@ -427,6 +427,7 @@ $TEST->addTests("testrun/rtti5", "", ['inferbox']);
 $TEST->addTests("testrun/rttioo", "", ['inferbox']);
 $TEST->addTests("testrun/rttioo2", "", ['inferbox']);
 $TEST->addTests("test/bind-formatstring", "EXTRAARGS=--assumePrintf", ['inferbox']);
+$TEST->addTests("test/bind-empty-chain", "", ['inferbox']);
 $TEST->addTests("test/bind-safe-wild", "EXTRAARGS=--assumePrintf", ['inferbox']);
 $TEST->addTests("testrun/pointerdiff", "", ['cil', 'inferbox', 'box']);
    $TEST->addBadComment("testrun/pointerdiff-inferbox", 
