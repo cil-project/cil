c535b7e116cc73657a2277d834bdc5b78f593ce3
Westley Weimer <weimer@cs.berkeley.edu>
2002-10-28 18:07:19 +0000

    Added a new "vsp" testcase that shows the error: undefined reference to `vsnprintf_ssvs'

---
+++ test/small1/vsp.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <testharness.h>
+
+static void
+ns_client_logv(void *client, void *category,
+           void *module, int level, const char *fmt, va_list ap)
+{
+        char msgbuf[2048];
+        char peerbuf[2048];
+
+        vsnprintf(msgbuf, sizeof(msgbuf), fmt, ap);
+}
+
+void
+ns_client_log(void *client, void *category, void *module, int level,
+    const char *fmt, ...)
+{
+  va_list ap;
+
+  va_start(ap,fmt);
+  ns_client_logv(client,category,module,level,fmt,ap);
+  va_end(ap); 
+}
+
+int main()
+{
+  int i;
+
+  ns_client_log(NULL, NULL, NULL, 0, 
+    "Hello, %s! 2+2=%d\n", "world", 4); 
+
+  return 0; 
+} 
--- test/testcil.pl
+++ test/testcil.pl
@@ -440,6 +440,7 @@ $TEST->addTests("testrun/oneret", "", ['inferbox']);
 $TEST->addTests("test/bind-too-many", "", ['inferbox']);
 $TEST->addTests("testrun/split1", "", ['inferbox']);
 $TEST->add3Tests("testrun/rmtmps-attr");
+$TEST->add3Tests("testrun/vsp");
 $TEST->addTests("test/bind-formatstring", "EXTRAARGS=--assumePrintf", ['inferbox']);
 $TEST->addTests("test/bind-empty-chain", "", ['inferbox']);
 $TEST->addTests("test/bind-safe-wild", "EXTRAARGS=--assumePrintf", ['inferbox']);
