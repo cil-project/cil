026d4846796d9f4e2016eae611ab5af4a78f5b38
George Necula <necula@cs.berkeley.edu>
2002-12-24 16:28:38 +0000

    Fixed a pattern in testsafec.pl (so that failsprintf's failure is recognized)

--- test/testcil.pl
+++ test/testcil.pl
@@ -491,7 +491,7 @@ $TEST->add2TestsFail("testmodel/noproto1", "", "Function pointer");
 $TEST->add2TestsFail("testmodel/noproto2", "", "Failure: Non-pointer");
 # $TEST->add2Tests("testmodel/noproto");
 
-$TEST->add2TestsFail("testrun/failsprintf1", "", "Failure .+: Ubound");
+$TEST->add2TestsFail("testrun/failsprintf1", "", "Failure: Ubound");
 $TEST->add2TestsFail("testrun/failsprintf2", "", "Failure: ");
 $TEST->add2TestsFail("testrun/failsprintf3", "", "Failure: Non-pointer");
 
