c1c9ee152e714928bdb3a33726798dcc2c146d2e
George Necula <necula@cs.berkeley.edu>
2003-01-08 02:43:35 +0000

    Fixed bug in the handling of compatible structs. Rewrote the wrapper for glob

--- test/testcil.pl
+++ test/testcil.pl
@@ -374,7 +374,6 @@ $TEST->add2Tests("testwrapper/wrapper1");
 $TEST->add2Tests("testwrapper/wrapperpoly");
 $TEST->addTests("test-bad-wrapper/wrapper1", "", ['inferbox']);
 $TEST->addTests("test-bad-wrapper/wrapper2", "", ['inferbox']);
-$TEST->addTests("test-bad/wrapper2", "", ['inferbox']);
 $TEST->addTests("testrun/decl1", "_GNUCC=1", ['cil']);
 $TEST->addTests("wes-hashtest", "", ['cil', 'inferbox']);
 $TEST->add3Tests("wes-rbtest", "");
