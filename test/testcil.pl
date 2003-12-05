f300e83b883f4e4883f038b5cb11f5360aa2878c
Westley Weimer <weimer@cs.berkeley.edu>
2003-12-05 22:41:53 +0000

    add a test case that checks wide character string literal escape sequence handling (and also that we can pass to the library functions for wide characters correctly)

--- test/testcil.pl
+++ test/testcil.pl
@@ -384,6 +384,7 @@ $TEST->add3Tests("testrun/wchar2");
 $TEST->add3Tests("testrun/wchar3");
 $TEST->add3Tests("testrun/wchar4");
 $TEST->addTests("testrun/wchar5", "", ['cil']);
+$TEST->add3Tests("testrun/wchar6"); 
 $TEST->add2Tests("testrun/escapes");
 $TEST->addTests("test-bad1/wchar-bad", "", ['cil']);
 $TEST->add3Tests("testrun/addrof", "MANUALBOX=1");
