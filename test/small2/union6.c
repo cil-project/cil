1fd63e3c050a4f111b3d16e57ff1486b8b4cd1d2
Matt Harren <matth@cs.berkeley.edu>
2004-02-27 01:55:59 +0000

    Fix a soundness bug in tagged unions that have fields larger than one word.  This fix uses memset to clear such fields, although we could probably optimize this if needed.

--- test/small2/union6.c
+++ test/small2/union6.c
@@ -16,5 +16,5 @@ int main() {
   u.f2 = 5; // now u.f1.a = 5
   u.f1.b = &i; // now the tag says that u.f1 is active
 
-  i = * u.f1.a; //ERROR(1): Null-pointer
+  i = * u.f1.a; //ERROR(1): Null pointer
 }
