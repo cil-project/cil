0ca2d45dfcf0bf3128ef6ddb6d9084e0e3a5506c
Matt Harren <matth@cs.berkeley.edu>
2004-07-29 05:30:21 +0000

    comment  IGNORE

--- ocamlutil/util.mli
+++ ocamlutil/util.mli
@@ -309,4 +309,8 @@ end
 
 (************************************************************************)
 
+(** This has the semantics of (=) on OCaml 3.07 and earlier.  It can
+   handle cyclic values as long as a structure in the cycle has a unique
+   name or id in some field that occurs before any fields that have cyclic
+   pointers. *)
 val equals: 'a -> 'a -> bool
