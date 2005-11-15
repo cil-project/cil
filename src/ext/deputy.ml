9860f2174487c6fc60adc316efdc2cdf88b76b83
Jeremy Condit <jcondit@cs.berkeley.edu>
2005-11-15 22:25:41 +0000

    added basic structure for type checker

--- src/ext/deputy.ml
+++ src/ext/deputy.ml
@@ -41,9 +41,95 @@ let debug : bool ref = ref false
 let verbose : bool ref = ref false
 let suppress : bool ref = ref false
 
-let analyzeFile (f: file) : unit =
-  E.log "If we'd implemented deputy, it would be running right now.\n";
-  ()
+let curFunc : fundec ref = ref dummyFunDec
+
+let compareTypes (t1 : typ) (t2 : typ) : bool =
+  (typeSig t1) = (typeSig t2)
+
+let checkTypes (t1 : typ) (t2 : typ) : unit =
+  if not (compareTypes t1 t2) then
+    E.log "%a: type mismatch\n" d_loc !currentLoc
+
+let rec checkExp (e : exp) : typ =
+  match e with
+  | Lval lv -> checkLval lv
+  | CastE (t, e') -> checkTypes t (checkExp e'); t
+  | _ -> typeOf e
+
+and checkLval (lv : lval) : typ =
+  typeOfLval lv
+
+let checkInstr (instr : instr) : unit =
+  currentLoc := get_instrLoc instr;
+  match instr with
+  | Call (lvo, fn, args, _) ->
+      begin
+        match checkExp fn with
+        | TFun (returnType, argInfo, varags, _) ->
+            let lvType =
+              match lvo with
+              | Some lv -> checkLval lv
+              | None -> voidType
+            in
+            checkTypes returnType lvType;
+            List.iter2
+              (fun (argName, argType, _) arg ->
+                 checkTypes argType (checkExp arg))
+              (argsToList argInfo)
+              args
+        | _ -> E.log "%a: calling non-function type\n" d_loc !currentLoc
+      end
+  | Set (lv, e, _) ->
+      checkTypes (checkLval lv) (checkExp e)
+  | Asm _ -> E.s (E.unimp "asm unsupported\n")
+
+let rec checkStmt (s : stmt) : unit =
+  currentLoc := get_stmtLoc s.skind;
+  match s.skind with
+  | Instr instrs ->
+      List.iter checkInstr instrs
+  | Return (eo, _) ->
+      begin
+        match eo with
+        | Some e ->
+            let returnType =
+              match !curFunc.svar.vtype with
+              | TFun (returnType, _, _, _) -> returnType
+              | _ -> E.s (E.bug "expected function type")
+            in
+            checkTypes returnType (checkExp e)
+        | None -> ()
+      end
+  | If (e, b1, b2, _) ->
+      checkTypes intType (checkExp e);
+      checkBlock b1;
+      checkBlock b2;
+  | Switch (e, b, _, _) ->
+      checkTypes intType (checkExp e);
+      checkBlock b
+  | Loop (b, _, _, _)
+  | Block b -> checkBlock b
+  | Goto _
+  | Break _
+  | Continue _ -> ()
+  | TryFinally _
+  | TryExcept _ -> E.s (E.unimp "exceptions not supported\n")
+
+and checkBlock (b : block) : unit =
+  List.iter checkStmt b.bstmts
+
+let checkFundec (fd : fundec) : unit =
+  curFunc := fd;
+  checkBlock fd.sbody;
+  curFunc := dummyFunDec
+
+let checkFile (f : file) : unit =
+  List.iter
+    (fun global ->
+       match global with
+       | GFun (fd, _) -> checkFundec fd
+       | _ -> ())
+    f.globals
 
 let feature : featureDescr = 
   { fd_name = "Deputy";
@@ -53,6 +139,6 @@ let feature : featureDescr =
       "--deputyverbose", Arg.Set verbose, "Enable verbose output for Deputy";
       "--deputysuppress", Arg.Set suppress, "Suppress some Deputy warnings";
     ];
-    fd_doit = analyzeFile;
+    fd_doit = checkFile;
     fd_post_check = true;
   } 
