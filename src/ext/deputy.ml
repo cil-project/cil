ae5ae22f4be9c064f776f804970228d66b522516
Matt Harren <matth@cs.berkeley.edu>
2005-11-15 03:26:07 +0000

    oh, I should probably check in the actual file while I'm at it.  IGNORE

---
+++ src/ext/deputy.ml
@@ -0,0 +1,58 @@
+(*
+ *
+ * Copyright (c) 2004, 
+ *  Jeremy Condit       <jcondit@cs.berkeley.edu>
+ *  George C. Necula    <necula@cs.berkeley.edu>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. The names of the contributors may not be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *)
+open Cil
+open Pretty
+module E = Errormsg
+
+let debug : bool ref = ref false
+let verbose : bool ref = ref false
+let suppress : bool ref = ref false
+
+let analyzeFile (f: file) : unit =
+  E.log "If we'd implemented deputy, it would be running right now.\n";
+  ()
+
+let feature : featureDescr = 
+  { fd_name = "Deputy";
+    fd_enabled = ref false;
+    fd_description = "Typecheck and instrument the program using Deputy.";
+    fd_extraopt = [
+      "--deputyverbose", Arg.Set verbose, "Enable verbose output for Deputy";
+      "--deputysuppress", Arg.Set suppress, "Suppress some Deputy warnings";
+    ];
+    fd_doit = analyzeFile;
+    fd_post_check = true;
+  } 
