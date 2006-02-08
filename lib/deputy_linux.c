76bee0180a1e06b39741d11f10a278cd3df1c4ee
Jeremy Condit <jcondit@cs.berkeley.edu>
2006-02-08 18:37:14 +0000

    added deputy strcpy implementation

--- lib/deputy_linux.c
+++ lib/deputy_linux.c
@@ -11,3 +11,11 @@ void deputy_fail(const char *what, const char *check,
 int deputy_strlen(const char *str) {
     return strlen(str);
 }
+
+char *deputy_strcpy(char *dest, const char *src) {
+    char *tmp = dest;
+    while ((*dest++ = *src++) != '\0') {
+        // do nothing
+    }
+    return tmp;
+}
