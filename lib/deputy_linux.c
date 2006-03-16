5049827e01974b1f56dbf9e38216e41e2a4463ba
Bill McCloskey <billm@cs.berkeley.edu>
2006-03-16 04:06:22 +0000

    Made the Deputy runtime functions compile as regparm(0) to avoid conflicts.

--- lib/deputy_linux.c
+++ lib/deputy_linux.c
@@ -1,18 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-void deputy_fail(const char *what, const char *check,
+asmlinkage void deputy_fail(const char *what, const char *check,
                  const char *file, int line) {
     printk(KERN_ALERT
            "Assertion \"%s\" failed in %s check at %s:%d.\n", 
            what, check, file, line);
 }
 
-int deputy_strlen(const char *str) {
+asmlinkage int deputy_strlen(const char *str) {
     return strlen(str);
 }
 
-char *deputy_strcpy(char *dest, const char *src) {
+asmlinkage char *deputy_strcpy(char *dest, const char *src) {
     char *tmp = dest;
     while ((*dest++ = *src++) != '\0') {
         // do nothing
@@ -20,7 +20,7 @@ char *deputy_strcpy(char *dest, const char *src) {
     return tmp;
 }
 
-char *deputy_strncpy(char *dest, const char *src, size_t count) {
+asmlinkage char *deputy_strncpy(char *dest, const char *src, size_t count) {
     char *tmp = dest;
     while (count >= 0) {
         if ((*tmp = *src) != 0) src++;
