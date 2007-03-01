c54092581b304baa73d64fa3c3fd8db21a6e0fdf
Matt Harren <matth@cs.berkeley.edu>
2007-03-01 02:55:52 +0000

    in the regression tests, check CIL's invariants after running Deputy.

--- src/check.ml
+++ src/check.ml
@@ -61,7 +61,7 @@ let valid = ref true
 
 let warn fmt =
   valid := false;
-  Cil.warn fmt
+  Cil.warn ("CIL invariant broken: "^^fmt)
 
 let warnContext fmt =
   valid := false;
@@ -301,16 +301,20 @@ and checkPointerType (t: typ) =
 
 
 and typeMatch (t1: typ) (t2: typ) = 
-  (* Allow mismatches in const-ness, so that string literals can be used
-     as char*s *)
-  if typeSigIgnoreConst t1 <> typeSigIgnoreConst t2 then
-    match unrollType t1, unrollType t2 with
-    (* Allow free interchange of TInt and TEnum *)
-      TInt (IInt, _), TEnum _ -> ()
-    | TEnum _, TInt (IInt, _) -> ()
-
-    | _, _ -> ignore (warn "Type mismatch:@!    %a@!and %a@!"
-                        d_type t1 d_type t2)
+  if !Cil.insertImplicitCasts then begin
+    (* Allow mismatches in const-ness, so that string literals can be used
+       as char*s *)
+    if typeSigIgnoreConst t1 <> typeSigIgnoreConst t2 then
+      match unrollType t1, unrollType t2 with
+        (* Allow free interchange of TInt and TEnum *)
+        TInt (IInt, _), TEnum _ -> ()
+      | TEnum _, TInt (IInt, _) -> ()
+          
+      | _, _ -> ignore (warn "Type mismatch:@!    %a@!and %a@!"
+                           d_type t1 d_type t2)
+  end else begin
+    (* Many casts are missing.  For now, just skip this check. *)
+  end
 
 and checkCompInfo (isadef: defuse) comp = 
   let fullname = compFullName comp in
