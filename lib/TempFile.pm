38e4e8f01f2cdd75972a3b7241bb7aa9364b590c
Matt Harren <matth@cs.berkeley.edu>
2008-02-14 17:48:43 +0000

    TempFile fix

--- lib/TempFile.pm
+++ lib/TempFile.pm
@@ -15,11 +15,11 @@ sub new {
     my ($proto, $basis, $suffix) = @_;
     my $class = ref($proto) || $proto;
 
-    my (undef, $filename) = tempfile('cil-XXXXXXXX',
-				     DIR => File::Spec->tmpdir,
-				     SUFFIX => ".$suffix",
-				     UNLINK => 1);
-
+    my ($fh, $filename) = tempfile('cil-XXXXXXXX',
+				   DIR => File::Spec->tmpdir,
+				   SUFFIX => ".$suffix",
+				   UNLINK => 1);
+    close($fh);
     my $self = $class->SUPER::new($basis, $filename);
     return $self;
 }
