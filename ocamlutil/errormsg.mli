94b8f841387a508132a80d9f87e4e68802ba89d3
Zachary R. Anderson <zra@cs.berkeley.edu>
2009-01-27 00:02:36 +0000

    Expose color escape strings

--- ocamlutil/errormsg.mli
+++ ocamlutil/errormsg.mli
@@ -47,6 +47,14 @@ val verboseFlag : bool ref
 (** Set to true if you want error and warning messages to be colored *)
 val colorFlag : bool ref
 
+val redEscStr : string
+val greenEscStr : string
+val yellowEscStr : string
+val blueEscStr : string
+val purpleEscStr : string
+val cyanEscStr : string
+val whiteEscStr : string
+
 (** Set to true if you want to see all warnings. *)
 val warnFlag: bool ref
 
