490f89a89326f084ae31d53c7b1fde28d7553cc0
Gabriel Kerneis <gabriel@kerneis.info>
2013-06-17 19:23:11 +0100

    Fix regression tests

--- test/testcil.pl
+++ test/testcil.pl
@@ -163,12 +163,12 @@ sub addToGroup {
 $TEST->newTest(
     Name => "!inittests0",
     Dir => "..",
-    Cmd => "$make setup",
+    Cmd => "$make cilly cillib",
     Group => ['ALWAYS']);
 $TEST->newTest(
     Name => "!inittests2",
     Dir => "..",
-    Cmd => "$make setup _GNUCC=1",
+    Cmd => "$make cilly cillib _GNUCC=1",
     Group => ['ALWAYS']);
 
 
