ad075be5065596968b20881954b0456c3a55ddfd
Gabriel Kerneis <gabriel@kerneis.info>
2013-08-22 18:56:40 +0100

    Restore some functions in Util module
    This partially reverts commit 3b3d57fb928cba8e2d95f8a4914030d6e0b5fe16.

--- ocamlutil/util.ml
+++ ocamlutil/util.ml
@@ -1,3 +1,22 @@
+(** Utility functions for Coolaid *)
+module E = Errormsg
+module H = Hashtbl
+module IH = Inthash
+
+let rec list_rev_append revxs ys =
+  begin match revxs with
+  | [] -> ys
+  | x::xs -> list_rev_append xs (x::ys)
+  end
+;;
+
+(* tail-recursive append: reverses xs twice *)
+let list_append (xs: 'a list) (ys: 'a list): 'a list =
+  match xs with (* optimize some common cases *)
+      [] -> ys
+    | [x] -> x::ys
+    | _ -> list_rev_append (List.rev xs) ys
+
 let list_array_map f l =
   Array.to_list (Array.map f (Array.of_list l))
  
@@ -25,6 +44,22 @@ let rec count_map f l ctr =
        else count_map f tl (ctr + 1))
  
 let list_map f l = count_map f l 0
+
+(* Memoize *)
+let memoize (h: ('a, 'b) Hashtbl.t) 
+            (arg: 'a) 
+            (f: 'a -> 'b) : 'b = 
+  try
+    Hashtbl.find h arg
+  with Not_found -> begin
+    let res = f arg in
+    Hashtbl.add h arg res;
+    res
+  end
+
+let valOf : 'a option -> 'a = function
+    None -> raise (Failure "Util.valOf")
+  | Some x -> x
 
 let equals x1 x2 : bool =
   (compare x1 x2) = 0
--- ocamlutil/util.mli
+++ ocamlutil/util.mli
@@ -1,4 +1,16 @@
-val list_map : ('a -> 'b) -> 'a list -> 'b list
+(** A bunch of generally useful functions *)
+
+val list_append: ('a list) -> ('a list) -> ('a list) (** tail-recursive append *)
+
+val list_map : ('a -> 'b) -> 'a list -> 'b list (** More efficient map *)
+
+val memoize: ('a, 'b) Hashtbl.t ->
+            'a ->
+            ('a -> 'b) -> 'b
+
+
+(** Get the value of an option.  Raises Failure if None *)
+val valOf : 'a option -> 'a
 
 (** This has the semantics of (=) on OCaml 3.07 and earlier.  It can
    handle cyclic values as long as a structure in the cycle has a unique
