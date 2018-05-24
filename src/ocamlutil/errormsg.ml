ce9f7edb28e031f48963dc8aa30c63dd8a8fb886
Zak Kincaid <zkincaid@gmail.com>
2018-05-23 20:29:10 -0400

    Replaced deprecated mutable String operations with Bytes equivalents

--- src/ocamlutil/errormsg.ml
+++ src/ocamlutil/errormsg.ml
@@ -209,18 +209,18 @@ let rem_quotes str = String.sub str 1 ((String.length str) - 2)
 let cleanFileName str = 
   let str1 = 
     if str <> "" && String.get str 0 = '"' (* '"' ( *) 
-    then rem_quotes str else str in
-  let l = String.length str1 in
+    then Bytes.of_string (rem_quotes str) else Bytes.of_string str in
+  let l = Bytes.length str1 in
   let rec loop (copyto: int) (i: int) = 
     if i >= l then 
-      String.sub str1 0 copyto
+      Bytes.sub_string str1 0 copyto
      else 
-       let c = String.get str1 i in
+       let c = Bytes.get str1 i in
        if c <> '\\' then begin
-          String.set str1 copyto c; loop (copyto + 1) (i + 1)
+          Bytes.set str1 copyto c; loop (copyto + 1) (i + 1)
        end else begin
-          String.set str1 copyto '/';
-          if i < l - 2 && String.get str1 (i + 1) = '\\' then
+          Bytes.set str1 copyto '/';
+          if i < l - 2 && Bytes.get str1 (i + 1) = '\\' then
               loop (copyto + 1) (i + 2)
           else 
               loop (copyto + 1) (i + 1)
