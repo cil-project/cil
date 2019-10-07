09a7b10c4151f4bcce5c39f9486a806446397d13
Michael Schwarz <michael.schwarz93@gmail.com>
2019-10-07 11:10:11 +0200

    Specify std=gnu90 for combine test cases.
    Test case combine21 is ok in GNU90-C, but invalid in GNU11.
    GNU90 used to be the default.
    Since GCC 5.1.0., default is GNU11, causing problems when using a newer GCC.

--- test/small1/combine21_1.c
+++ test/small1/combine21_1.c
@@ -1,5 +1,9 @@
 static int gflag;
 
+// This is not considered correct code in GNU11, but is ok in GNU90.
+// -std=gnu90 used to be the default, but since GCC 5.1.0 it is -std=gnu11
+// To account for this, -std=gnu90 was added to the regressions tests
+// To make it GNU11, add extern
 __inline void testit ( int flag )
 {
         gflag = flag;
