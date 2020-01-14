4bbbed73e33744db61a871c4608228e2b3ac9142
Michael Schwarz <michael.schwarz93@gmail.com>
2020-01-14 13:43:15 +0100

    Extend tgmath.h test

--- test/small1/c99-tgmath.c
+++ test/small1/c99-tgmath.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <tgmath.h>
-#include <math.h>
 #include "testharness.h"
 
 int main(void)
 {
-    float v = fabs(1.0f);
+    float f = fabs(1.0f);
+    double d = fabs(1.0);
+    long l = fabs(1.0l);
+
+    if(f != 1.0f)
+        E(1);
+
+    if(d != 1.0)
+        E(2);
+
+    if(l != 1.0l)
+        E(3);
+
+    return 0;
 }
