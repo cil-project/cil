76642217018ad8f0ea25a7c96686dff929a64c88
Michael Schwarz <michael.schwarz93@gmail.com>
2021-05-21 14:49:50 +0200

    Remove tests that asserted behavior that changes between gcc-10 and gcc-11

--- test/small1/const11.c
+++ test/small1/const11.c
@@ -16,21 +16,18 @@ int main () {
   uint64 d = 1 << 33 ;               // 0, since 1 is an int.
   uint64 e = ((uint64)1) << 33 ;  // 2,0000,0000h  (2**33)
   int f = 1 << 64;          // Don't fold this.
-  int g = 1 << -1;          // Don't fold this.  treated as 0.
 
 
-  printf("a=%d, b=%d, c=%d, d=%d:%u, e=%d:%u, f=%d, g=%d\n", 
-         a, b, c, 
-         (int)(d>>32), (unsigned int)d, (int)(e>>32), (unsigned int)e, 
+  printf("a=%d, b=%d, c=%d, d=%d:%u, e=%d:%u, f=%d, g=%d\n",
+         a, b, c,
+         (int)(d>>32), (unsigned int)d, (int)(e>>32), (unsigned int)e,
          f, g);
 
   //right shift:
   aa = ((uint64)0x200000000LL) >> 33; // 1
   bb = ((uint64)0x200000000LL) >> 65; // Don't fold this.  gcc treats it as 0.
-  cc = 3 >> -1;                        // Don't fold this. gcc treats it as 0.
 
-
-  printf("aa=%d, bb=%d, cc=%d\n", 
+  printf("aa=%d, bb=%d, cc=%d\n",
          aa, bb, cc);
 
   if (a != 512) E(2);
@@ -39,11 +36,9 @@ int main () {
   if (d != 0) E(5);
   if (e != ((uint64)0x200000000)) E(6);
   if (f != 0) E(7);
-  if (g != 0) E(8);
 
-  if (aa != 1) E(9);
-  if (bb != 0) E(10);
-  if (cc != 6) E(11);
-  
+  if (aa != 1) E(8);
+  if (bb != 0) E(9);
+
   SUCCESS;
 }
