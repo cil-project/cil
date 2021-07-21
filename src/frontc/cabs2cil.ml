48b3b19571fcbcc92f81a8922e83b6c613b111dc
Simmo Saan <simmo.saan@gmail.com>
2021-07-21 11:20:30 +0300

    Add column debugging to convLoc

--- src/frontc/cabs2cil.ml
+++ src/frontc/cabs2cil.ml
@@ -194,7 +194,7 @@ let transparentUnionArgs : (int * typ) list ref = ref []
 let debugLoc = false
 let convLoc (l : cabsloc) =
   if debugLoc then
-    ignore (E.log "convLoc at %s: line %d, byte %d\n" l.filename l.lineno l.byteno);
+    ignore (E.log "convLoc at %s: line %d, byte %d, column %d\n" l.filename l.lineno l.byteno l.columnno);
   {line = l.lineno; file = l.filename; byte = l.byteno; column = l.columnno}
 
 
