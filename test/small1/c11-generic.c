dd8f2f2df84f7e6fa060e2a1db394a21d3f68cb1
Simmo Saan <simmo.saan@gmail.com>
2021-09-13 18:03:39 +0300

    Add C11 generic string literal test

--- test/small1/c11-generic.c
+++ test/small1/c11-generic.c
@@ -2,6 +2,7 @@
 #define type1(x) _Generic((x), char: 1, unsigned int:2, default:0)
 #define type2(x) _Generic((x), char: 1, unsigned int:2, const int:3, default:0)
 #define type3(x) _Generic((x), int:1, const int:2, default:0)
+#define type4(x) _Generic((x), char*:1, char[4]:0)
 
 int main() {
     unsigned char v_uchar;
@@ -15,12 +16,14 @@ int main() {
 
     if(type2(v_int) != 0) { E(4); }
     if(type2(v_intconst) != 0) { E(5); }
-    
+
     if(type3(v_int) != 1) { E(6); }
     if(type3(v_intconst) != 1) { E(7); }
 
     if(type3((const int)v_int) != 1) { E(8); }
     if(type3((const int)v_intconst) != 1) { E(9); }
+
+    if((type4("abcd")) != 1) { E(10); }
 
     SUCCESS;
 }
