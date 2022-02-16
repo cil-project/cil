cf7af764ffb83510671a594ca9acbd9b4df755c0
Michael Schwarz <michael.schwarz93@gmail.com>
2022-02-16 13:58:32 +0100

    add test for attribuites in decllist

---
+++ test/small1/attr-in-decllist.c
@@ -0,0 +1,7 @@
+int __attribute__((unused))version,j;
+int i, ret, __attribute__((unused))version2, nb_curves;
+int i2, ret2, __attribute__((unused))(version3), nb_curves2;
+
+int main(void) {
+    return 0;
+}
