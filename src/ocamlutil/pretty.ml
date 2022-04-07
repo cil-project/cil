5455f2afb5b395e866e2089c5977999a20d472d0
Simmo Saan <simmo.saan@gmail.com>
2022-04-07 19:10:30 +0300

    Fix L, l, n format specs in Pretty

--- src/ocamlutil/pretty.ml
+++ src/ocamlutil/pretty.ml
@@ -725,8 +725,8 @@ let gprintf (finish : doc -> 'b)
               invalid_arg ("dprintf: unimplemented format "
 			   ^ (String.sub format i (j-i+1)));
 	    let j' = succ j in (* eat the d,i,x etc. *)
-	    let format_spec = Bytes.of_string "% " in
-	    Bytes.set format_spec 1 (fget j'); (* format_spec = "%x", etc. *)
+	    let format_spec = Bytes.of_string "%L " in
+	    Bytes.set format_spec 2 (fget j'); (* format_spec = "%Lx", etc. *)
             Obj.magic(fun n ->
               collect (dctext1 acc
                          (Printf.sprintf (Scanf.format_from_string (Bytes.to_string format_spec) "%Lx") n))
@@ -735,8 +735,8 @@ let gprintf (finish : doc -> 'b)
 	    if j != i + 1 then invalid_arg ("dprintf: unimplemented format "
 					    ^ (String.sub format i (j-i+1)));
 	    let j' = succ j in (* eat the d,i,x etc. *)
-	    let format_spec = Bytes.of_string "% " in
-	    Bytes.set format_spec 1 (fget j'); (* format_spec = "%x", etc. *)
+	    let format_spec = Bytes.of_string "%l " in
+	    Bytes.set format_spec 2 (fget j'); (* format_spec = "%lx", etc. *)
             Obj.magic(fun n ->
               collect (dctext1 acc
                          (Printf.sprintf (Scanf.format_from_string (Bytes.to_string format_spec) "%lx") n))
@@ -745,8 +745,8 @@ let gprintf (finish : doc -> 'b)
 	    if j != i + 1 then invalid_arg ("dprintf: unimplemented format "
 					    ^ (String.sub format i (j-i+1)));
 	    let j' = succ j in (* eat the d,i,x etc. *)
-	    let format_spec = Bytes.of_string "% " in
-	    Bytes.set format_spec 1 (fget j'); (* format_spec = "%x", etc. *)
+	    let format_spec = Bytes.of_string "%n " in
+	    Bytes.set format_spec 2 (fget j'); (* format_spec = "%nx", etc. *)
             Obj.magic(fun n ->
               collect (dctext1 acc
                          (Printf.sprintf (Scanf.format_from_string (Bytes.to_string format_spec) "%nx") n))
