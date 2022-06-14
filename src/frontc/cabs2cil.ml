bbd2561561b65ca1a4e2582d17f24081a8c95f59
Simmo Saan <simmo.saan@gmail.com>
2022-06-14 15:01:52 +0300

    Change intermediate exp/init side effect locs to synthetic

--- src/frontc/cabs2cil.ml
+++ src/frontc/cabs2cil.ml
@@ -840,6 +840,52 @@ module BlockChunk =
 
     let isNotEmpty (c: chunk) = not (isEmpty c)
 
+    (** Change all stmt and instr locs to synthetic, except the first one.
+        Expressions/initializers that expand to multiple instructions cannot have intermediate locations referenced. *)
+    let synthesizeLocs (c: chunk): chunk =
+      (* ignore (Pretty.eprintf "synthesizeLocs %a\n" d_chunk c); *)
+      let doLoc l =
+        (* ignore (Pretty.eprintf "synthesizeLoc %a in %a\n" d_loc l d_chunk c); *)
+        {l with synthetic = true}
+      in
+      let doInstr: instr -> instr = function
+        | Set (l, e, loc, eloc) -> Set (l, e, doLoc loc, doLoc eloc)
+        | VarDecl (v, loc) -> VarDecl (v, doLoc loc)
+        | Call (l, f, a, loc, eloc) -> Call (l, f, a, doLoc loc, doLoc eloc)
+        | Asm (a, b, c, d, e, loc) -> Asm (a, b, c, d, e, doLoc loc)
+      in
+      let doInstrs ~first = function
+        | [] -> []
+        | x :: xs when first -> x :: List.map doInstr xs
+        | xs -> List.map doInstr xs
+      in
+      let rec doStmt ~first s =
+        let skind' =
+          let doLoc = if first then doLoc else Fun.id in
+          match s.skind with
+          | Instr xs -> Instr (doInstrs ~first xs)
+          | Return (e, loc) -> Return (e, doLoc loc)
+          | Goto (s, loc) -> Goto (s, doLoc loc)
+          | ComputedGoto (e, loc) -> ComputedGoto (e, doLoc loc)
+          | Break loc -> Break (doLoc loc)
+          | Continue loc -> Continue (doLoc loc)
+          | If (c, t, f, loc, eloc) -> If (c, doBlock ~first:false t, doBlock ~first:false f, doLoc loc, doLoc eloc)
+          | Switch (e, b, s, loc, eloc) -> Switch (e, doBlock ~first:false b, doStmts ~first:false s, doLoc loc, doLoc eloc)
+          | Loop (b, loc, eloc, s1, s2) -> Loop (doBlock ~first:false b, doLoc loc, doLoc eloc, Option.map (doStmt ~first:false) s1, Option.map (doStmt ~first:false) s2)
+          | Block b -> Block (doBlock ~first b)
+        in
+        {s with skind = skind'}
+      and doBlock ~first b =
+        {b with bstmts = doStmts ~first b.bstmts}
+      and doStmts ~first = function
+        | [] -> []
+        | x :: xs -> doStmt ~first x :: List.map (doStmt ~first:false) xs
+      in
+      match c.stmts, c.postins with
+      | [], [] -> c
+      | [], postins -> {c with postins = List.rev (doInstrs ~first:true (List.rev postins))}
+      | stmts, postins -> {c with stmts = doStmts ~first:true stmts; postins = List.rev (doInstrs ~first:false (List.rev postins))}
+
     let i2c (i: instr) =
       { empty with postins = [i] }
 
@@ -3352,9 +3398,9 @@ and doExp (asconst: bool)   (* This expression is used as a constant *)
                 (se: chunk) (e: exp) (t: typ) : chunk * exp * typ =
     match newWhat with
       ADrop
-    | AType -> (se, e, t)
+    | AType -> (synthesizeLocs se, e, t)
     | AExpLeaveArrayFun ->
-        (se, e, t) (* It is important that we do not do "processArrayFun" in
+        (synthesizeLocs se, e, t) (* It is important that we do not do "processArrayFun" in
                     * this case. We exploit this when we process the typeOf
                     * construct *)
     | AExp _ ->
@@ -3363,20 +3409,20 @@ and doExp (asconst: bool)   (* This expression is used as a constant *)
         ignore (E.log "finishExp: e'=%a, t'=%a\n"
            d_exp e' d_type t');
 *)
-        (se, e', t')
+        (synthesizeLocs se, e', t')
 
     | ASet (lv, lvt) -> begin
         (* See if the set was done already *)
         match e with
           Lval(lv') when lv == lv' ->
-            (se, e, t)
+            (synthesizeLocs se, e, t)
         | _ ->
             let (e', t') = processArrayFun e t in
             let (t'', e'') = castTo t' lvt e' in
 (*
             ignore (E.log "finishExp: e = %a\n  e'' = %a\n" d_plainexp e d_plainexp e'');
 *)
-            (se +++ (Set(lv, e'', !currentLoc, !currentExpLoc)), e'', t'')
+            (synthesizeLocs (se +++ (Set(lv, e'', !currentLoc, !currentExpLoc))), e'', t'')
     end
   in
   let findField (n: string) (fidlist: fieldinfo list) : offset =
@@ -5909,8 +5955,8 @@ and doDecl (isglobal: bool) : A.definition -> chunk = function
           acc
         end else
           match spec_res with
-          | Some spec_res -> acc @@ createLocal spec_res name
-          | None -> acc @@ createAutoLocal name
+          | Some spec_res -> acc @@ synthesizeLocs (createLocal spec_res name)
+          | None -> acc @@ synthesizeLocs (createAutoLocal name)
       in
       let res = List.fold_left doOneDeclarator empty nl in
 (*
