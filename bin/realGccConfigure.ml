f9299529448ccdf57af691deceae80acd4867188
Simmo Saan <simmo.saan@gmail.com>
2022-07-16 22:48:16 +0300

    Add hack for finding real gcc, especially on MacOS

---
+++ bin/realGccConfigure.ml
@@ -0,0 +1,31 @@
+module C = Configurator.V1
+
+let bad_gcc_version_regexp = Str.regexp_case_fold "clang\\|apple\\|darwin"
+
+let is_bad_gcc_version stdout =
+  match Str.search_forward bad_gcc_version_regexp stdout 0 with
+  | exception Not_found -> false
+  | _ -> true
+
+let is_real_gcc c gcc =
+  let result: C.Process.result = C.Process.run c gcc ["--version"] in
+  result.exit_code = 0 && not (is_bad_gcc_version result.stdout)
+
+let gccs = [
+  "gcc";
+  "gcc-12";
+  "gcc-11";
+  "gcc-10";
+  "gcc-9";
+  "gcc-8";
+  "gcc-7";
+]
+
+let () =
+  C.main ~name:"real-gcc" (fun c ->
+      match List.find_opt (is_real_gcc c) gccs with
+      | Some gcc ->
+        C.Flags.write_lines "real-gcc" [gcc]
+      | None ->
+        failwith "couldn't find real gcc"
+    )
